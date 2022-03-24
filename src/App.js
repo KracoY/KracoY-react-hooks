60b6c26213d3a5fcd25a2a26bd12e862ba0fb52d
KracoY <enderyuruk9932@gmail.com>
2022-03-24 11:38:19 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Container, Button } from "react-bootstrap";
 
 function App() {
+  // useeffect usestate useref usecontext >> react hooks
+
+  const [posts, setPosts] = useState([]); // saved json data in here
+  const [IndexNumber, setIndexNumber] = useState(1);
+  const[singlePost, setSinglePost] = useState({});
+  useEffect(() => {
+    console.log("hello world");
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        //  console.log(data)
+        setPosts(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const nextPost = (e) => {
+    e.preventDefault();
+    console.log(IndexNumber);
+    setIndexNumber(IndexNumber + 1); //0,1,2,3,4
+    fetch(`https://jsonplaceholder.typicode.com/posts/${IndexNumber}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setSinglePost(data);
+      });
+  };
+
+  const prevPost = (e) => {
+    e.preventDefault();
+    console.log(IndexNumber);
+    if (IndexNumber <= 0) {
+      alert("No post found!!!");
+    } else {
+      setIndexNumber(IndexNumber - 1); //0,1,2,3,4
+      fetch(`https://jsonplaceholder.typicode.com/posts/${IndexNumber}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setSinglePost(data);
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="text-center mt-5">
+      <h3>{singlePost.title}</h3>
+
+      <Button variant={"warning"} className="mx-1" onClick={prevPost}>
+        {" "}
+        Previous Post
+      </Button>
+      <Button variant={"warning"} onClick={nextPost}>
+        Next post
+      </Button>
+
+      <div className="di">{singlePost.body}</div>
+
+      {/* {
+      posts.map((post,index)=>(
+        <div key={index}>
+          <p>{console.log(post)}</p>
+          <p>{post.title}</p>
+        </div>
+      ))
+    } */}
+    </Container>
   );
 }
 
